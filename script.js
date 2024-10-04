9f88bd3fc19065f1c4d1ffc56fe8889c259e6858
Bharath Krishnan J <bharathkrishnan.jbk@gmail.com>
2024-10-04 12:53:14 +0530

    Add files via upload
    Chocolate Factory Packing Problem
    Problem Description
    In a chocolate factory, packets of chocolates are represented as an array of integers. Each integer value corresponds to a packet, where 0 indicates an empty packet, and any positive integer represents a filled packet with chocolates. The task is to rearrange the array such that all empty packets (i.e., 0s) are moved to the end of the array while maintaining the order of the filled packets.
    
    Example
    Given:
    
    Input:
    
    Number of packets, 
    𝑁
    =
    8
    N=8
    Array of packets, 
    𝑎
    𝑟
    𝑟
    =
    [
    4
    ,
    5
    ,
    0
    ,
    1
    ,
    9
    ,
    0
    ,
    5
    ,
    0
    ]
    arr=[4,5,0,1,9,0,5,0]
    Output:
    
    Rearranged array, 
    𝑎
    𝑟
    𝑟
    =
    [
    4
    ,
    5
    ,
    1
    ,
    9
    ,
    5
    ,
    0
    ,
    0
    ,
    0
    ]
    arr=[4,5,1,9,5,0,0,0]
    Requirements
    Input: An integer array containing a mixture of positive integers and zeros.
    Output: A modified array where all zeros are shifted to the end, while the order of non-zero integers remains unchanged.
    Implementation
    The solution involves iterating through the array, moving non-zero elements to the front, and counting the zeros. Finally, the remaining positions in the array are filled with zeros.

---
+++ script.js
@@ -0,0 +1,18 @@
+
+            let n=prompt("Enter n");
+            let arr=[];
+            let c=0;
+            for(i=0;i<n;i++)
+            {
+            arr[i]=prompt("enter element")
+            if(arr[i]==0)
+            {
+                c=c+1;
+            }
+            else{
+                console.log("there is no zero")
+            }
+            }
+console.log(c)
+
+       
